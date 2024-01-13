e12618ffc198e2f0dc89a5a1a31b19c2168e142b
Marwane Ahansal <ahansalmarwane@gmail.com>
2024-01-13 05:16:17 +0100

    check for hex values in request target

--- src/Request/Helpers.cpp
+++ src/Request/Helpers.cpp
@@ -29,6 +29,9 @@ bool Helpers::decodeURI(string& requestURI)
             if (i + 2 >= requestURI.length())
                 return false;
             hex = requestURI.substr(i + 1, 2);
+            for (size_t i = 0; i < hex.length(); i++)
+                if (!isxdigit(hex[i]))
+                    return false;
             char c = (char)strtol(hex.c_str(), NULL, 16);
             decodedURI += c;
             i += 2;
